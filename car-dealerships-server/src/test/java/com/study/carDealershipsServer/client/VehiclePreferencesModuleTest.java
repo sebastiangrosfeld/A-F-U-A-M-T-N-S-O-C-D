4abfef537d9b236a703eb8a62c475cce63f0482f
Sebastian Grosfeld <sebekf9@wp.pl>
2024-11-16 19:20:04 +0100

    remove unused imports

--- car-dealerships-server/src/test/java/com/study/carDealershipsServer/client/VehiclePreferencesModuleTest.java
+++ car-dealerships-server/src/test/java/com/study/carDealershipsServer/client/VehiclePreferencesModuleTest.java
@@ -19,8 +19,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
